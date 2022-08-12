fac954652a84beb2806bf6f815a819e5fd1e3e9e
kukwa clovis <kukwaclovisngong3@gmail.com>
2022-08-12 23:40:12 +0100

    config: installing and setting up vue3 + vite project

---
+++ client/src/App.vue
@@ -0,0 +1,15 @@
+<script setup>
+import HelloWorld from "./components/HelloWorld.vue";
+</script>
+
+<template>
+  <HelloWorld />
+</template>
+
+<style scoped>
+/* .logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+} */
+</style>
---
+++ client/src/components/HelloWorld.vue
@@ -0,0 +1,11 @@
+<script setup>
+</script>
+
+<template>
+  <div>
+    <h1>welcome to vue and vite project startup</h1>
+  </div>
+</template>
+
+<style scoped>
+</style>
